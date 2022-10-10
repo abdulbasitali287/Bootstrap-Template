157ec9fe0d8dbbff48d6226711021a312589592d
AbdulBasit <=>
2022-10-10 16:36:59 +0500

    bootstrap ashtra template fourth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-let a = document.getElementsByClassName('accordion-section');
-a.addEventListener('click',()=>{
-    
-})
-
+// let x = document.getElementsByClassName('frequently-button');
+let x = document.querySelectorAll('.frequently-button')
+let con_to_array = Array.from(x);
+console.log(con_to_array.length)
+for (let index = 0; index < con_to_array.length; index++) {
+    console.log(con_to_array[index])
+    con_to_array[index].addEventListener('click',function(e) {
+        
+    })
+}
