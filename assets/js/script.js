a147cefcd4e33b31589a6599cac5e998ed62814d
AbdulBasit <=>
2022-10-14 16:55:17 +0500

    bootstrap ashtra template fifth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
-// let x = document.getElementsByClassName('frequently-button');
-let x = document.querySelectorAll('.frequently-button')
-let con_to_array = Array.from(x);
-console.log(con_to_array.length)
-for (let index = 0; index < con_to_array.length; index++) {
-    console.log(con_to_array[index])
-    con_to_array[index].addEventListener('click',function(e) {
-        
-    })
-}
+// let a = document.getElementById('navigation-menu').classList;
+// a.forEach(element => {
+//     console.log(element)
+// });
+// let a = document.getElementById('navigation-menu').firstElementChild;
+// let a = document.getElementById('navigation-menu').lastElementChild;
+// let a = document.getElementById('navigation-menu').firstElementChild.innerHTML;
+// let a = document.getElementById('navigation-menu').parentElement;
+// let a = document.getElementById('navigation-menu').childElementCount;
+// let a = document.getElementById('navigation-menu').childElementCount;
+//         console.log(a)
+let menu = document.getElementById('navigation-menu');
+let toggleMenu = document.getElementById('menu-bars');
+toggleMenu.addEventListener('click', function(){
+        menu.classList.forEach(element => {
+            if (element === "d-none") {
+                menu.setAttribute('class','d-md-flex justify-content-end flex-wrap flex-shrink-1');
+            }else{
+                menu.setAttribute('class','d-none')
+            }
+        });
+});
+// function showMenu() {
+// }
+// console.log(toggleMenu)
